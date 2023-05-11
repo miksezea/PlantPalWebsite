e4030d33103fd36aafbfe3a397153f4d8b831a5d
mettedahllarsen <mettedahllarsen@gmail.com>
2023-05-11 09:25:01 +0200

    Added Navigation Bar

--- js/index.js
+++ js/index.js
@@ -3,13 +3,32 @@ const baseUrl = "https://plantpalweb.azurewebsites.net/api/sensordatas"
 Vue.createApp({
     data() {
         return {
-            sensorDatas: [],
+            plants: [],
+            error: null, 
+
+            Id: null, 
+            Name: "",
+            Type: "",
         }
     },
+
+    async created() {
+        console.log("Created method called");
+        try {
+            const response = await axios.get(baseUrl);
+            this.plants = await response.data;
+            this.error = null;
+        } catch (e) {
+            this.plants = [];
+            this.error = e.message;
+        }
+    },
+
     methods: {
         getAllSensorDatas() {
             this.helperGetAndShow(baseUrl)
         },
+
         async helperGetAndShow(url) {
             try {
                 const response = await axios.get(url)
@@ -18,6 +37,7 @@ Vue.createApp({
                 alert(ex.message)
             }
         },
+        
         clearList() {
             this.sensorDatas = [];
         }
